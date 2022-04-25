46d65b997cb3d7e75c26a5e30fb575553a3d02f7
Asha jyothi <Aashajyothi-UI>
2022-04-25 18:49:12 +0530

    update packages

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static(__dirname + '/dist/app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/app'));
 });
 
 // Start the app by listening on the default Heroku port
