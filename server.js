c62c70ffedd268d55be1d2815cdf43f86cd9f607
Asha jyothi <Aashajyothi-UI>
2022-04-25 12:17:25 +0530

    added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static('./dist/APP'));
+app.use(express.static('./dist/APP/src'));
 app.get('/*',(req,res) => 
-res.sendFile('index.html',{root:'dist/APP/'}),);
+res.sendFile('index.html',{root:'dist/APP/src'}),);
 app.listen(process.env.PORT || 8080);
