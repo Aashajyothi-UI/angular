3da0fac80ea5764414df21c7c7eecea157d02eaf
Asha jyothi <Aashajyothi-UI>
2022-04-25 13:20:06 +0530

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// app.use(express.static('./dist/APP/src'));
+// app.get('/*',(req,res) => 
+// res.sendFile('index.html',{root:'dist/APP/src'}),);
+// app.listen(process.env.PORT || 8080);
+
+
+
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static('./dist/APP/src'));
-app.get('/*',(req,res) => 
-res.sendFile('index.html',{root:'dist/APP/src'}),);
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
