f264cd25d3c0b575d7cb659ec5e9e67e5639e3cb
Asha jyothi <Aashajyothi-UI>
2022-05-12 15:31:50 +0530

    changed url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseURL:"https://crudcrud.com/api/3fa7e39eec0949aca02a48283c88f870/user",     
+  loginurl:" http://localhost:3000/Userlist"
 };
